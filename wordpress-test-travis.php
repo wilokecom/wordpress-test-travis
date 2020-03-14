9f688db8a0ff86f1cf4be1ab64c33e825b7c9134
nguyen_dttn <contact.wilcity@gmail.com>
2020-03-14 21:14:21 +0700

    First commit

---
+++ wordpress-test-travis.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WordPress Test Travis
+ * Plugin URI: https://wiloke.com
+ * Author: Wiloke
+ * Author URI: https://wiloke.com
+ * Description: This is my travis test
+ * Version: 1.0
+ */
