74425578c8e7f17ad9346cb42c4fe8681727922a
nguyen_dttn <contact.wilcity@gmail.com>
2020-03-16 23:06:22 +0700

    Test Failed

--- tests/Controllers/ValueFormatControllerTest.php
+++ tests/Controllers/ValueFormatControllerTest.php
@@ -29,6 +29,6 @@ class ValueFormatControllerTest extends TestCase
     {
         array_push($stack, 'foo');
         
-        $this->assertIsArray($stack);
+        $this->assertIsArray(1);
     }
 }
